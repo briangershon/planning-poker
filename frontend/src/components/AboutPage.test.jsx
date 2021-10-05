dff9b8c1a3f0e39e3e6157d34a1077266aa7c6c5
Brian Gershon <briangershon@users.noreply.github.com>
2021-10-04 20:13:06 -0700

    Use AboutPage for sample test.

--- frontend/src/components/AboutPage.test.jsx
+++ frontend/src/components/AboutPage.test.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { expect } from 'chai';
-import HomePage from './HomePage';
+import AboutPage from './AboutPage';
 
 afterEach(cleanup);
 
 describe('This will test HomePage', () => {
   it('renders', () => {
-    const { getByText } = render(<HomePage />);
-    expect(document.body.contains(getByText('Home')));
+    const { getByText } = render(<AboutPage />);
+    expect(document.body.contains(getByText('About')));
   });
 });
