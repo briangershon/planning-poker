58473068af8148206c87467163503eab8ca1b78c
Brian Gershon <briangershon@users.noreply.github.com>
2021-10-30 14:23:10 -0700

    Hide game page if not logged in.

--- frontend/src/components/PlayGame.jsx
+++ frontend/src/components/PlayGame.jsx
@@ -3,7 +3,6 @@ import { useHistory, useParams } from 'react-router-dom';
 const { API_URL } = import.meta.env;
 import { deleteGameId } from '../store/userSlice';
 
-
 import Players from './Players';
 import styles from './PlayGame.module.css';
 
@@ -19,6 +18,7 @@ import {
 function PlayGame() {
   const game = useSelector((state) => state.poker);
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
 
   let { gameId } = useParams();
 
@@ -35,76 +35,81 @@ function PlayGame() {
     const data = await response.json();
     history.push(`/`);
     dispatch(deleteGameId(gameId));
-  };
+  }
 
   return (
     <div>
-      Game ID is {gameId}.
-      <Players
-        you={game.you}
-        players={game.players}
-        showCards={game.showCards}
-      />
-      <div>
-        <p>Choose your card:</p>
-        <ul className={styles.vote}>
-          <li>
-            <button onClick={() => dispatch(vote('XS'))}>XS</button>
-          </li>
-          <li>
-            <button onClick={() => dispatch(vote('S'))}>S</button>
-          </li>
-          <li>
-            <button onClick={() => dispatch(vote('M'))}>M</button>
-          </li>
-          <li>
-            <button onClick={() => dispatch(vote('L'))}>L</button>
-          </li>
-          <li>
-            <button onClick={() => dispatch(vote('XL'))}>XL</button>
-          </li>
-          <li>
-            <button onClick={() => dispatch(vote('XXL'))}>XXL</button>
-          </li>
-          <li>
-            <button onClick={() => dispatch(vote('?'))}>?</button>
-          </li>
-          <li>
-            <button onClick={() => dispatch(vote(null))}>Clear</button>
-          </li>
-        </ul>
-      </div>
-      {game.showCards ? (
-        <button onClick={() => dispatch(hideCards())}>Hide Cards</button>
-      ) : (
-        <button onClick={() => dispatch(showCards())}>Show Cards</button>
+      {!isLoggedIn && <div>Please login to participate in game.</div>}
+      {isLoggedIn && (
+        <div>
+          Game ID is {gameId}.
+          <Players
+            you={game.you}
+            players={game.players}
+            showCards={game.showCards}
+          />
+          <div>
+            <p>Choose your card:</p>
+            <ul className={styles.vote}>
+              <li>
+                <button onClick={() => dispatch(vote('XS'))}>XS</button>
+              </li>
+              <li>
+                <button onClick={() => dispatch(vote('S'))}>S</button>
+              </li>
+              <li>
+                <button onClick={() => dispatch(vote('M'))}>M</button>
+              </li>
+              <li>
+                <button onClick={() => dispatch(vote('L'))}>L</button>
+              </li>
+              <li>
+                <button onClick={() => dispatch(vote('XL'))}>XL</button>
+              </li>
+              <li>
+                <button onClick={() => dispatch(vote('XXL'))}>XXL</button>
+              </li>
+              <li>
+                <button onClick={() => dispatch(vote('?'))}>?</button>
+              </li>
+              <li>
+                <button onClick={() => dispatch(vote(null))}>Clear</button>
+              </li>
+            </ul>
+          </div>
+          {game.showCards ? (
+            <button onClick={() => dispatch(hideCards())}>Hide Cards</button>
+          ) : (
+            <button onClick={() => dispatch(showCards())}>Show Cards</button>
+          )}
+          <div>
+            <button
+              onClick={() => {
+                dispatch(endGame());
+                history.push('/');
+              }}
+            >
+              End Game
+            </button>
+          </div>
+          <hr />
+          <div>
+            <button
+              onClick={() =>
+                dispatch(
+                  addPlayer({
+                    name: 'New',
+                    value: null,
+                  })
+                )
+              }
+            >
+              Add Player
+            </button>
+            <button onClick={deleteCurrentGame}>Delete Game</button>
+          </div>
+        </div>
       )}
-      <div>
-        <button
-          onClick={() => {
-            dispatch(endGame());
-            history.push('/');
-          }}
-        >
-          End Game
-        </button>
-      </div>
-      <hr />
-      <div>
-        <button
-          onClick={() =>
-            dispatch(
-              addPlayer({
-                name: 'New',
-                value: null,
-              })
-            )
-          }
-        >
-          Add Player
-        </button>
-        <button onClick={deleteCurrentGame}>Delete Game</button>
-      </div>
     </div>
   );
 }
