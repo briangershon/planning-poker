13a73b1818108c18852513306931fd4115d6cfe4
Brian Gershon <briangershon@users.noreply.github.com>
2021-11-15 00:04:53 -0800

    Update About page for v2.0.0 and websocket support

--- frontend/src/components/AboutPage.jsx
+++ frontend/src/components/AboutPage.jsx
@@ -6,11 +6,22 @@ function AboutPage() {
       <h1>About Planning Poker</h1>
 
       <p>
-        The game is an entry in the{' '}
+        This game began as an entry in the{' '}
         <a href="https://challenge.developers.cloudflare.com" target="_blank">
           Cloudflare Developer Challenge
         </a>
         .
+      </p>
+      <p>
+        See the{' '}
+        <a
+          href="https://github.com/briangershon/planning-poker/blob/main/CHANGELOG.md"
+          target="_blank"
+        >
+          Changelog
+        </a>{' '}
+        for version v1.0.0 submitted for the challenge, as well as the full
+        websocket version that was added in v2.0.0.
       </p>
 
       <p>
@@ -20,17 +31,20 @@ function AboutPage() {
       </p>
 
       <p>
-        This project uses Cloudflare Workers, Workers KV, Durable Objects, and
-        Cloudflare Pages. Also React, Redux Toolkit, TypeScript and Snowpack.
+        This project uses Cloudflare Workers, Workers KV, Durable Objects,
+        Websockets and Cloudflare Pages. Also React, Redux Toolkit, TypeScript
+        and Snowpack.
       </p>
 
       <p>
         This was a good fit for Cloudflare <strong>Durable Objects</strong>{' '}
-        since each game manages state for multiple players.
+        since each game manages state for multiple players, as well as{' '}
+        <strong>Websockets</strong> for real-time player updates.
       </p>
 
       <p>
-        The code is available at{' '}
+        The code is available at:
+        <br />
         <a
           href="https://github.com/briangershon/planning-poker"
           target="_blank"
