cf28aff8dd4af1c47c5f76ef2b36ec8f1934f54e
Brian Gershon <briangershon@users.noreply.github.com>
2021-12-04 21:19:07 -0800

    Show message when URL copied to clipboard. Update changelog.

--- frontend/src/components/GameInvite.jsx
+++ frontend/src/components/GameInvite.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './GameInvite.module.css';
 
-export function GameInvite({ relativeGameInviteUrl, gameInviteUrl }) {
+export function GameInvite({ gameInviteUrl }) {
+  const [message, updateMessage] = useState('');
+
   function onClick() {
     navigator.clipboard.writeText(gameInviteUrl);
-    alert(`${gameInviteUrl} url copied! Sent to other players.`);
+    updateMessage(`URL copied to clipboard!`);
+    setTimeout(() => {
+      updateMessage('');
+    }, 2000);
   }
 
   return (
     <>
       <div className={styles.container}>
-        {/* <div className={styles.label}>Invite players: </div> */}
+        <div className={styles.message}>{message}</div>
         <button className={styles.button} onClick={onClick}>
           Invite players via <img src="/link-45deg.svg" />
         </button>
-        {/* <div className={styles.shortLink}>{gameInviteUrl}</div> */}
       </div>
     </>
   );
